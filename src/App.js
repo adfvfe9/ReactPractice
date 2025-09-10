bb49f4d38cf69b3c6e9992906a1012ab54cf9813
공은성 <adfvfe9@gmail.com>
2025-09-10 19:50:19 +0900

    20250910 - 2 / localStorage, onClick 사용법

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const print = () => {
+    console.log("내용");
+  };
+  const NewSpace = () => {
+    alert("내용")
+  }
+  function abc() {
+    console.error("에러")
+  }
+  let cnt = 0
+  const [count, setCount] = useState(localStorage.getItem("a"))
+  localStorage.setItem("a", count)
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +30,21 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          p태그
+        </p>
+        <a href="./App2">링크</a>
+        <img src={logo} className="App-logo2"></img>
+        <button onClick={print}>버튼</button><br></br>
+        <button onClick={abc}>버튼</button><br></br>
+        <button onClick={NewSpace}>버튼</button><br></br>
+        <button onClick={() => setCount(Number(count) + 1)}>증가</button>
+        <button onClick={() => setCount(count - 1)}>감소</button>
+        <p>현재 숫자: {localStorage.getItem("a")}</p>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
       </header>
     </div>
   );
